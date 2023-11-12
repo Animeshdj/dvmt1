abbf1a0c26f2647168808d95038f7603fa9ce644
Animeshdj <animeshstick.567@gmail.com>
2023-11-12 21:40:34 +0530

    fixes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+const nav_menu = document.querySelector('.main-nav');
+const cart_btn = document.querySelector('.cart');
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active')
+    mobile_menu.classList.toggle('is-active')
+    nav_menu.classList.toggle('is-active')
+    cart_btn.classList.toggle('is-active')
 })
