9f050f244857321ab546684f00fdcf8c0e7944f1
Animeshdj <animeshstick.567@gmail.com>
2023-11-19 09:23:10 +0530

    added section 2

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 const nav_menu = document.querySelector('.main-nav');
 const cart_btn = document.querySelector('.cart');
+const main_sec = document.querySelector('main');
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active')
     mobile_menu.classList.toggle('is-active')
     nav_menu.classList.toggle('is-active')
     cart_btn.classList.toggle('is-active')
+    main_sec.classList.toggle('is-active')
 })
